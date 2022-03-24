4c8a946424fda0e51b45d787b5f52f4c9b47b182
rzryanz <95293422+rzryanz@users.noreply.github.com>
2022-03-24 22:46:55 +0800

    Create ex_02_05.py

---
+++ python/py4e/chap_02_variables/ex_02_05.py
@@ -0,0 +1,6 @@
+inp = input("Please enter temperature in Celsius: ")
+
+celsius_degree = float(inp)
+fahrenheit_degree = celsius_degree * 9 / 5 + 32
+
+print("The temperature in Fahrenheit is:", fahrenheit_degree)
